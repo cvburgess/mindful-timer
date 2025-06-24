a6b3a6b9930be83c56ec28f8fd217c6ddb9936ea
Charles Burgess <consulting@cvburgess.com>
2025-06-24 14:25:56 -0400

    Sample sounds when selected

--- Mindful Timer/SettingsView.swift
+++ Mindful Timer/SettingsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct SettingsView: View {
   @Environment(\.dismiss) private var dismiss
@@ -13,8 +14,25 @@ struct SettingsView: View {
   @AppStorage("roundStartSound") private var roundStartSound = "ding"
   @AppStorage("breakStartSound") private var breakStartSound = "none"
   @AppStorage("sessionEndSound") private var sessionEndSound = "bowl"
+  @State private var audioPlayer: AVAudioPlayer?
 
   private let soundOptions = ["none", "bell", "bowl", "ding", "gong"]
+
+  private func playSound(_ soundName: String) {
+    guard soundName != "none" else { return }
+
+    guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else {
+      print("Could not find \(soundName).wav file")
+      return
+    }
+
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOf: url)
+      audioPlayer?.play()
+    } catch {
+      print("Error playing sound: \(error)")
+    }
+  }
 
   var body: some View {
     NavigationView {
@@ -27,17 +45,26 @@ struct SettingsView: View {
               Text(sound.capitalized).tag(sound)
             }
           }
+          .onChange(of: roundStartSound) { _, newValue in
+            playSound(newValue)
+          }
 
           Picker("Break Start", selection: $breakStartSound) {
             ForEach(soundOptions, id: \.self) { sound in
               Text(sound.capitalized).tag(sound)
             }
           }
+          .onChange(of: breakStartSound) { _, newValue in
+            playSound(newValue)
+          }
 
           Picker("Session End", selection: $sessionEndSound) {
             ForEach(soundOptions, id: \.self) { sound in
               Text(sound.capitalized).tag(sound)
             }
+          }
+          .onChange(of: sessionEndSound) { _, newValue in
+            playSound(newValue)
           }
         }
       }
