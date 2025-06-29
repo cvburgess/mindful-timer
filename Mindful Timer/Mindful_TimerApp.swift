23ff0348469ebe6c26061c2d553e83f358beb0bb
Charles Burgess <consulting@cvburgess.com>
2025-06-29 18:11:29 -0400

    Remove dead code

--- Mindful Timer/Mindful_TimerApp.swift
+++ Mindful Timer/Mindful_TimerApp.swift
@@ -5,26 +5,13 @@
 //  Created by Charles Burgess on 6/22/25.
 //
 
-import SwiftData
 import SwiftUI
 
 @main
 struct Mindful_TimerApp: App {
-  var sharedModelContainer: ModelContainer = {
-    let schema = Schema([])
-    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-    do {
-      return try ModelContainer(for: schema, configurations: [modelConfiguration])
-    } catch {
-      fatalError("Could not create ModelContainer: \(error)")
-    }
-  }()
-
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
-    .modelContainer(sharedModelContainer)
   }
 }
